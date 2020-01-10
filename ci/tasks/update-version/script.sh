d826e5fdfa776526759a4273c61a10e9d84c6a3d
Marcin Rejdych <marcin.rejdych@relayr.io>
2020-01-10 10:37:28 +0100

    git secrets from arguments

--- ci/tasks/update-version/script.sh
+++ ci/tasks/update-version/script.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/env bash
 
+GITHUB_USER=$1
+GITHUB_USER=$2
+
 function add_credentials_to_remote() {
     git config --global user.email "git@localhost"
     git config --global user.name "git"
-    set +x
     git checkout master
     local remote
     remote=$(git remote show origin -n | grep "Fetch URL" | awk '{print $3}')
     remote=$(echo ${remote} | sed 's|://|://'${GITHUB_USER}':'${GITHUB_PASS}'@|')
     git remote set-url origin ${remote}
-    set -x
 }
 
 add_credentials_to_remote
@@ -19,9 +20,7 @@ if [[ -z "$VERSION" ]]; then
     VERSION="0.0.0"
 fi
 
-set +e
 LOG=$(git log --pretty=format:%s --decorate=no ${VERSION}..)
-set -e
 
 if echo ${LOG} | grep -iqF "#MAJOR"; then
     major=$(echo ${VERSION} | cut -d. -f1)
