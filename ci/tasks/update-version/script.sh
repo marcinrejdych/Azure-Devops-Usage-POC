c43ea3f1139b1b90a2b316bd26914684f7dc2de0
Marcin Rejdych <marcin.rejdych@relayr.io>
2020-01-10 09:28:53 +0100

    update-version task

---
+++ ci/tasks/update-version/script.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+function add_credentials_to_remote() {
+    set +x
+    local remote
+    remote=$(git remote show origin -n | grep "Fetch URL" | awk '{print $3}')
+    remote=$(echo ${remote} | sed 's|://|://'${GITHUB_USER}':'${GITHUB_PASS}'@|')
+    git remote set-url origin ${remote}
+    set -x
+}
+
+add_credentials_to_remote
+
+VERSION=$(cat ./version)
+if [[ -z "$VERSION" ]]; then
+    VERSION="0.0.0"
+fi
+
+set +e
+LOG=$(git log --pretty=format:%s --decorate=no ${VERSION}..)
+set -e
+
+if echo ${LOG} | grep -iqF "#MAJOR"; then
+    major=$(echo ${VERSION} | cut -d. -f1)
+    ((major=major+1))
+    VERSION="${major}.0.0"
+elif echo ${LOG} | grep -iqF "#MINOR"; then
+    major=$(echo ${VERSION} | cut -d. -f1)
+    minor=$(echo ${VERSION} | cut -d. -f2)
+    ((minor=minor+1))
+    VERSION="${major}.${minor}.0"
+else
+    major=$(echo ${VERSION} | cut -d. -f1)
+    minor=$(echo ${VERSION} | cut -d. -f2)
+    patch=$(echo ${VERSION} | cut -d. -f3)
+    ((patch=patch+1))
+    VERSION="${major}.${minor}.${patch}"
+fi
+
+echo ${VERSION} > ./version
+
+git add version
+git commit -m "[ci skip] Bumped version to ${VERSION}"
+git push
