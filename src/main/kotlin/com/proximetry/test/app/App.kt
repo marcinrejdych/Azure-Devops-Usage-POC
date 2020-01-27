c0c808d9d2cf887e37b425d8b4f635b461e5848a
Marcin Rejdych <marcin.rejdych@relayr.io>
2020-01-27 09:06:17 +0100

    add dependency to test library

--- src/main/kotlin/com/proximetry/test/app/App.kt
+++ src/main/kotlin/com/proximetry/test/app/App.kt
@@ -1,12 +1,20 @@
 package com.proximetry.test.app
 
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import spark.Spark.get
 import spark.Spark.port
 
 class App {
+    companion object {
+        val log: Logger = LoggerFactory.getLogger(App::class.java)
+    }
+
     val greeting: String
         get() {
-            return "Hello world."
+            val msg = Lib.generateMessage()
+            log.info("Generated message: $msg")
+            return msg
         }
 }
 
@@ -15,5 +23,6 @@ fun main(args: Array<String>) {
 
     get("/hello") { _, _ ->
         App().greeting
+
     }
 }
